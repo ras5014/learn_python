92c1885b81f2b65f6498bcdff0e3c63ff3d80cf3
Ramananda Samantaray <ramanandas@asiapac.altair.com>
2024-01-25 20:09:48 +0530

    First Push

---
+++ 01intro.py
@@ -0,0 +1,21 @@
+print("Hello World!")
+
+# Variables
+a = 6.2
+print(a)
+a = "Pilu"
+print(a)
+a = None
+print(a)
+
+# Typecasting
+f = "5"
+print(type(f))
+print(int(f)+1)
+
+# Operators
+num1 = 11
+num2 = 2
+print(f"{num1} / {num2} = {num1 // num2}") # Floor Division
+print(f"{num1} to the power {num2} = {num1 ** num2}") # Power
+
