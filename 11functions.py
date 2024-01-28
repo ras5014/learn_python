dc3030d37d483ee9911a34d3745919f532981826
ras5014 <ramanandasamantaray@gmail.com>
2024-01-29 00:26:51 +0530

    Updates Added

---
+++ 11functions.py
@@ -0,0 +1,13 @@
+def greetHello(name):
+    print(f"Hello {name}")
+
+
+greetHello("Pilu")
+
+
+def letterGenerator(name, date):
+    st = f"Hello {name},\n\tYou are selected!\n{date}"
+    print(st)
+
+
+letterGenerator("Silu", "12/12/2020")
